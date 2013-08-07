45a9a14aa8fede4831aeffa7fae1fa0f04d93ed1
Andrew Lovett-Barron <andrew@relaystudio.com>
2013-08-07 00:40:08 -0400

    UI

--- src/autoContainer.cpp
+++ src/autoContainer.cpp
@@ -56,3 +56,38 @@ void AutoContainer::update() {
 void AutoContainer::draw() {
     container->draw(loc.x, loc.y);
 }
+
+void Container::setupGUI() {
+    gui = new ofxUICanvas(0,0,container->getWidth(),container->getHeight());
+    gui->setAutoDraw(false);
+    gui->addImageButton("IMAGE BUTTON", "img/button_home.png", false, 44, 85, container->getWidth()-44, 0);
+    ofAddListener(gui->newGUIEvent,this,&Container::guiEvent);
+}
+
+
+void Container::drawGUI() {
+    gui->draw();
+}
+
+void Container::updateGUI() {
+    
+}
+
+void Container::exit() {
+    gui->saveSettings("GUI/guiSettings.xml");
+    delete gui;
+}
+
+void Container::guiEvent(ofxUIEventArgs &e) {
+    string name = e.widget->getName();
+	int kind = e.widget->getKind();
+    
+    if(kind == OFX_UI_WIDGET_IMAGEBUTTON)
+    {
+        ofxUIImageButton *button = (ofxUIImageButton *) e.widget;
+        cout << name << "\t value: " << button->getValue() << endl;
+        close();
+    }
+}
+
+
